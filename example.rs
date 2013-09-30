7b5793687307371b6f60e71bd9aa45461eb40d84
Jason E. Aten <j.e.aten@gmail.com>
2013-09-29 18:15:38 -0700

    partial update to rust 0.8

--- example.rs
+++ example.rs
@@ -1,50 +1,54 @@
 extern mod std;
-extern mod msgpack;
+extern mod extra;
 
-use core::path::Path;
-use core::hashmap::linear::LinearMap;
+use std::path::Path;
+use std::hashmap;
+use std::hashmap::HashMap;
 
-use std::time;
-use std::serialize::{Decoder, Decodable, Encoder, Encodable};
+use extra::time;
+use extra::serialize::{Decoder, Decodable, Encoder, Encodable};
 
-#[auto_encode]
-#[auto_decode]
+mod msgpack;
+
+
+#[deriving(Encodable,Decodable)]
+
 struct Blah {
   f: u8,
   g: u16,
   i: ~str,
   a: ~[u32],
-  c: LinearMap<u32, u32>
+  c: HashMap<u32, u32>
 }
 
 fn decod(bytes: &[u8]) {
-  let a: ~[~Blah] = do io::with_bytes_reader(bytes) |rd| {
+  let a: ~[~Blah] = do std::io::with_bytes_reader(bytes) |rd| {
     let parser = msgpack::Decoder::new(rd);
     Decodable::decode(&parser)
   };
-  io::println(fmt!("%?", a));
+  printfln!("%?", a);
 }
 
 fn main() {
   {
-    let res = io::buffered_file_writer(&Path("test.msgpack"));
+    let res = std::io::buffered_file_writer(&Path("test.msgpack"));
     if res.is_ok() {
-      let mut c = LinearMap::new();
+      let mut c = HashMap::new();
       c.insert(1_u32, 2_u32);
 
       let enc = msgpack::Encoder { wr: res.unwrap() };
-      let blah = Blah { f: 1, g: 2, i: ~"hallo", a: ~[], c: copy c };
-      let blub = Blah { f: 5, g: 1, i: ~"nochwas", a: ~[1,2,3], c: copy c };
+      let blah = Blah { f: 1, g: 2, i: ~"hallo", a: ~[], c: c };
+      let blub = Blah { f: 5, g: 1, i: ~"nochwas", a: ~[1,2,3], c:  c };
       let b = ~[blah, blub];
       b.encode(&enc);
       //5.encode(&enc);
-      io::println("OK");
+      println!("OK");
     }
   }
 
-  let bytes: ~[u8] = result::unwrap(io::read_whole_file(&Path("test.msgpack")));
+  let bytes: ~[u8] = std::io::read_whole_file(&Path("test.msgpack")).unwrap();
   let b = time::precise_time_ns();
   decod(bytes);
   let total = time::precise_time_ns() - b;
-  io::println(fmt!("%?", total / 1000000));
+  printfln!("%?", total / 1000000);
 }
