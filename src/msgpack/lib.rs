d79580d62c00ac3be388c4fcf680cb0bdb78c837
Michael Neumann <mneumann@ntecs.de>
2014-01-03 19:01:39 +0100

    again rustpkg

--- src/msgpack/lib.rs
+++ src/msgpack/lib.rs
@@ -1,29 +1,8 @@
-#[crate_id = "msgpack"]
+#[crate_id = "github.com/mneumann/rust-msgpack"]
 #[desc = "msgpack.org implementation for Rust"]
 #[license = "MIT/ASL2"]
 #[crate_type = "lib"]
 
 extern mod extra;
 
-use std::io::mem::{MemReader,MemWriter,with_mem_writer};
-use encoder::Encoder;
-use decoder::Decoder;
-use extra::serialize::{Encodable,Decodable};
-
-mod utils;
-mod encoder;
-mod decoder;
-mod parser;
-
-pub fn to_msgpack<'a, T: Encodable<Encoder<'a>>>(t: &T) -> ~[u8] {
-  with_mem_writer(|wr: &mut MemWriter| {
-    let mut encoder = Encoder::new(wr);
-    t.encode(&mut encoder);
-  })
-}
-
-pub fn from_msgpack<'a, T: Decodable<Decoder<'a>>>(bytes: ~[u8]) -> T {
-  let mut rd = MemReader::new(bytes);
-  let mut decoder = Decoder::new(&mut rd);
-  Decodable::decode(&mut decoder)
-}
+pub mod msgpack;
