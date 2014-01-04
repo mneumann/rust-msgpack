d9bf5646667fb9163a40c8f54c2cee25e9175151
Michael Neumann <mneumann@ntecs.de>
2014-01-04 01:47:34 +0100

    Extend parser for new msgpack spec
    In the new spec, the old "Raw" type becomes now a String.
    Furthermore a Binary type is added and a user defined
    Extended type.

--- src/msgpack/parser.rs
+++ src/msgpack/parser.rs
@@ -11,7 +11,9 @@ pub enum Value {
   Unsigned(u64),
   Float(f32),
   Double(f64),
-  Raw(~[u8])
+  String(~[u8]),
+  Binary(~[u8]),
+  Extended(i8, ~[u8])
 }
 
 /// A structure to decode Msgpack from a reader into a Value.
@@ -38,37 +40,66 @@ impl<'a> Parser<'a> {
     self.rd.read_bytes(len)
   }
 
+  fn parse_ext(&mut self, len: uint) -> Value {
+    let typ = self.rd.read_i8();
+    if typ < 0 { fail!("Reserved type") }
+    let data = self.rd.read_bytes(len);
+    Extended(typ, data)
+  }
+
   pub fn parse(&mut self) -> Value {
-    let c = self.rd.read_byte().unwrap();
+    let c: u8 = self.rd.read_byte().unwrap();
     match c {
-      0x00 .. 0x7f => Unsigned(c as u64),
-      0x80 .. 0x8f => self.parse_map(c as uint & 0x0F),
-      0x90 .. 0x9f => self.parse_array(c as uint & 0x0F),
-      0xa0 .. 0xbf => Raw(self._read_raw(c as uint & 0x1F)),
       0xc0         => Nil,
+
       0xc1         => fail!(~"Reserved"),
+
       0xc2         => Boolean(false),
       0xc3         => Boolean(true),
-      0xc4 .. 0xc9 => fail!(~"Reserved"),
-      0xca         => Float(utils::conv_float(self.rd.read_be_u32())),
-      0xcb         => Double(utils::conv_double(self.rd.read_be_u64())),
+
+      0x00 .. 0x7f => Unsigned(c as u64),
       0xcc         => Unsigned(self.rd.read_u8() as u64),
       0xcd         => Unsigned(self.rd.read_be_u16() as u64),
       0xce         => Unsigned(self.rd.read_be_u32() as u64),
       0xcf         => Unsigned(self.rd.read_be_u64()),
+
       0xd0         => Integer(self.rd.read_i8() as i64),
       0xd1         => Integer(self.rd.read_be_i16() as i64),
       0xd2         => Integer(self.rd.read_be_i32() as i64),
       0xd3         => Integer(self.rd.read_be_i64()),
-      0xd4 .. 0xd9 => fail!(~"Reserved"),
-      0xda         => { let b = self.rd.read_be_u16() as uint; Raw(self._read_raw(b)) },
-      0xdb         => { let b = self.rd.read_be_u32() as uint; Raw(self._read_raw(b)) },
+      0xe0 .. 0xff => Integer((c as i8) as i64),
+
+      0xca         => Float(utils::conv_float(self.rd.read_be_u32())),
+      0xcb         => Double(utils::conv_double(self.rd.read_be_u64())),
+
+      0xa0 .. 0xbf => String(self._read_raw((c as uint) & 0x1F)),
+      0xd9         => { let b = self.rd.read_u8() as uint; String(self._read_raw(b)) },
+      0xda         => { let b = self.rd.read_be_u16() as uint; String(self._read_raw(b)) },
+      0xdb         => { let b = self.rd.read_be_u32() as uint; String(self._read_raw(b)) },
+
+      0xc4         => { let b = self.rd.read_u8() as uint; Binary(self._read_raw(b)) },
+      0xc5         => { let b = self.rd.read_be_u16() as uint; Binary(self._read_raw(b)) },
+      0xc6         => { let b = self.rd.read_be_u32() as uint; Binary(self._read_raw(b)) },
+
+      0x90 .. 0x9f => self.parse_array((c as uint) & 0x0F),
       0xdc         => { let b = self.rd.read_be_u16() as uint; self.parse_array(b) },
       0xdd         => { let b = self.rd.read_be_u32() as uint; self.parse_array(b) },
+     
+      0x80 .. 0x8f => self.parse_map((c as uint) & 0x0F),
       0xde         => { let b = self.rd.read_be_u16() as uint; self.parse_map(b) },
       0xdf         => { let b = self.rd.read_be_u32() as uint; self.parse_map(b) },
-      0xe0 .. 0xff => Integer((c as i8) as i64),
-      _            => fail!(~"Invalid")
+
+      0xd4         => self.parse_ext(1),
+      0xd5         => self.parse_ext(2),
+      0xd6         => self.parse_ext(4),
+      0xd7         => self.parse_ext(8),
+      0xd8         => self.parse_ext(16),
+      0xc7         => { let b = self.rd.read_u8() as uint; self.parse_ext(b) },
+      0xc8         => { let b = self.rd.read_be_u16() as uint; self.parse_ext(b) },
+      0xc9         => { let b = self.rd.read_be_u32() as uint; self.parse_ext(b) },
+
+      // XXX: This is only here to satify Rust's pattern checker.
+      _            => fail!("Fatal")
     }
   }
 }
