98b1956f50f64cf7a7aae02210a30e0debc0e124
Michael Neumann <mneumann@ntecs.de>
2014-01-20 16:58:00 +0100

    Merge all into one file

--- src/examples/value/main.rs
+++ src/examples/value/main.rs
@@ -5,6 +5,6 @@ use std::os::args;
 fn main() {
   let contents = File::open(&Path::new(args()[1])).read_to_end();
   println!("{:?}", contents);
-  let a: msgpack::value::Value = msgpack::from_msgpack(contents);
+  let a: msgpack::Value = msgpack::from_msgpack(contents);
   println!("{:?}", a);
 }
