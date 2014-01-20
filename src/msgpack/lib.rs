8a46af3720fbd2465ae2c7898b3b7bd7b3954517
Michael Neumann <mneumann@ntecs.de>
2014-01-20 17:08:37 +0100

    Move test cases into test.rs

--- src/msgpack/lib.rs
+++ src/msgpack/lib.rs
@@ -12,8 +12,6 @@ use std::io::{MemReader,MemWriter};
 use extra::serialize;
 use extra::serialize::{Encodable,Decodable};
 
-use std::hashmap::HashMap; // for tests
-
 pub enum Value {
   Nil,
   Boolean(bool),
@@ -680,88 +678,4 @@ pub fn from_msgpack<'a, T: Decodable<Decoder<'a>>>(bytes: ~[u8]) -> T {
   let mut rd = MemReader::new(bytes);
   let mut decoder = Decoder::new(&mut rd);
   Decodable::decode(&mut decoder)
-}
-
-
-#[test]
-mod tests {
-
-#[test]
-fn test_circular_str() {
-  assert_eq!(~"", from_msgpack(to_msgpack(&~"")));
-  assert_eq!(~"a", from_msgpack(to_msgpack(&~"a")));
-  assert_eq!(~"abcdef", from_msgpack(to_msgpack(&~"abcdef")));
-}
-
-#[test]
-fn test_circular_int() {
-  assert_eq!(123, from_msgpack(to_msgpack(&123)));
-  assert_eq!(-123, from_msgpack(to_msgpack(&-123)));
-}
-
-#[test]
-fn test_circular_float() {
-  let v = -1243.111;
-  assert_eq!(v, from_msgpack(to_msgpack(&v)));
-}
-
-#[test]
-fn test_circular_bool() {
-  assert_eq!(true, from_msgpack(to_msgpack(&true)));
-  assert_eq!(false, from_msgpack(to_msgpack(&false)));
-}
-
-#[test]
-fn test_circular_list() {
-  let v = ~[1, 2, 3];
-  assert_eq!(v.clone(), from_msgpack(to_msgpack(&v)));
-}
-
-#[test]
-fn test_circular_map() {
-  let mut v = HashMap::new();
-  v.insert(1, 2);
-  v.insert(3, 4);
-  assert_eq!(v.clone(), from_msgpack(to_msgpack(&v)));
-}
-
-#[test]
-fn test_circular_option() {
-  let mut v = Some(1);
-  let w : Option<int> = from_msgpack(to_msgpack(&v));
-
-  assert_eq!(w, v);
-
-  v = None;
-  assert_eq!(v.clone(), from_msgpack(to_msgpack(&v)));
-}
-
-#[test]
-fn test_circular_char() {
-  let a: char = 'a';
-  assert_eq!(a, from_msgpack(to_msgpack(&a)))
-}
-
-#[deriving(Encodable,Decodable,Eq)]
-struct S {
-  f: u8,
-  g: u16,
-  i: ~str,
-  a: ~[u32],
-  c: HashMap<u32, u32>
-}
-
-#[test]
-fn test_circular_struct() {
-  let mut c = HashMap::new();
-  c.insert(1_u32, 2_u32);
-  c.insert(2_u32, 3_u32);
-
-  let s1 = S { f: 1, g: 2, i: ~"foo", a: ~[], c: c.clone() };
-  let s2 = S { f: 5, g: 1, i: ~"bar", a: ~[1,2,3], c: c.clone() };
-  let s = ~[s1, s2];
-
-  assert_eq!(s, from_msgpack(to_msgpack(&s)))
-}
-
 }
