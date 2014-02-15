f331922bca6baa4990e2c2628581620cd8045e4f
Michael Neumann <mneumann@ntecs.de>
2014-02-16 00:22:03 +0100

    Fix example

--- src/examples/value/main.rs
+++ src/examples/value/main.rs
@@ -1,17 +1,11 @@
 extern mod msgpack = "msgpack#0.1";
 use std::io::{File};
-use std::io::{IoResult, IoError};
 use std::os::args;
 
 fn main() {
-  let contents = File::open(&Path::new(args()[1])).read_to_end();
-  println!("{:?}", contents);
+  let contents = File::open(&Path::new(args()[1])).read_to_end().unwrap();
+  debug!("{:?}", contents);
 
-  // let a = match(contents) {
-    // _ 
-  //   IoResult(r)    => msgpack::from_msgpack(contents),
-  //   IoError(msg) => fail!(msg)
-  // };
-  // // let a: msgpack::Value = msgpack::from_msgpack(contents).unwrap();
-  // println!("{:?}", a);
+  let a: msgpack::Value = msgpack::from_msgpack(contents);
+  debug!("{:?}", a);
 }
