c132a07b4b4a8c8c93a572a95c37e7c3abdd2f4a
Tamir Duberstein <tamird@gmail.com>
2015-03-19 17:48:46 -0700

    Update example to avoid `old_id`

--- examples/value.rs
+++ examples/value.rs
@@ -1,13 +1,15 @@
-#![feature(env, io, path)]
-
 extern crate msgpack;
 
-use std::old_io::File;
 use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let contents = File::open(&Path::new(args[1].clone())).read_to_end().ok().unwrap();
+    let filename = &args[1];
+    let mut contents = Vec::new();
+    File::open(&Path::new(filename)).unwrap().read_to_end(&mut contents).unwrap();
     println!("{:?}", contents);
 
     /* todo
